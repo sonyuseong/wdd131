fea00a39b8d6f595b3dd9469eafa6930012e9a06
sonyuseong <Son21009@byui.edu>
2025-05-10 11:34:51 -0600

    Cool pics 1

---
+++ coolpics/coolpics.js
@@ -0,0 +1,9 @@
+// coolpics.js 파일
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("menu-links");
+
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("show");
+  });
+});
